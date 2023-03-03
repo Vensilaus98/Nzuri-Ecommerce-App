80ef70c401db7cc56c57bc5bdfdc481d8cbe097a
Vensilaus98 <vensilauskimario98@gmail.com>
2023-03-03 07:59:06 +0300

    Fxied my cart page

--- lib/screens/my_cart_screen/my_cart_screen.dart
+++ lib/screens/my_cart_screen/my_cart_screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../colors/colors.dart';
+import '../../models/product_model.dart';
 import '../../strings/my_cart_screen_strings.dart';
 import '../home_screen/home_screen.dart';
 
@@ -35,11 +36,20 @@ class CartScreen extends StatefulWidget {
 
 class _CartScreenState extends State<CartScreen> {
 
+  Future<List<Product>>? myCartProducts;
+
+  @override
+  void initState() {
+    myCartProducts = Provider.of<ProductProvider>(context, listen: false)
+        .getAllCartItemsFromDatabase();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
 
     //Get all cart products
-    final myCartProducts = context.watch<ProductProvider>().cartProducts;
+    // final myCartProducts = context.watch<ProductProvider>().cartProducts;
 
     //Get total cart amount
     final double total = context.watch<ProductProvider>().totalCartAmount;
@@ -95,121 +105,123 @@ class _CartScreenState extends State<CartScreen> {
           ),
           elevation: 0),
       body: SingleChildScrollView(
-        child: Container(
-              width: double.infinity,
-              height: 100.h,
-              decoration: BoxDecoration(color: kBackgroundColor),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(3.w, 5.w, 3.w, 2.w),
-                child: ListView.builder(
-                    itemCount: myCartProducts.length,
-                    itemBuilder: (context, index) {
-                      final product = myCartProducts[index];
-
-                      return Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: Card(
-                            color: kProductCardColor,
-                            elevation: 2.0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(2.w)),
-                            child: Padding(
-                              padding: EdgeInsets.only(right: 2.w),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        width: 20.w,
-                                        height: 15.h,
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(4.w))),
-                                        child: Image.network(
-                                          product.image,
-                                          width: double.infinity,
-                                          height: 19.h,
-                                          fit: BoxFit.cover,
+        child: Consumer<ProductProvider>(
+          builder: (context, productProvider, child) => Container(
+                width: double.infinity,
+                height: 100.h,
+                decoration: BoxDecoration(color: kBackgroundColor),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(3.w, 5.w, 3.w, 2.w),
+                  child: ListView.builder(
+                      itemCount: productProvider.cartProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = productProvider.cartProducts;
+        
+                        return Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Card(
+                              color: kProductCardColor,
+                              elevation: 2.0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(2.w)),
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 2.w),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          width: 20.w,
+                                          height: 15.h,
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(4.w))),
+                                          child: Image.network(
+                                            product[index].image,
+                                            width: double.infinity,
+                                            height: 19.h,
+                                            fit: BoxFit.cover,
+                                          ),
                                         ),
-                                      ),
-                                      SizedBox(
-                                        width: 3.w,
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.only(top: 4.w),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                              width: 45.w,
-                                              child: Text(
-                                                product.title,
-                                                style: GoogleFonts.robotoSlab(
-                                                    color: kTextColor,
-                                                    fontSize: 12.sp,
-                                                    fontWeight:
-                                                        FontWeight.w600),
-                                                softWrap: false,
-                                                maxLines: 2,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              height: 2.w,
-                                            ),
-                                            Row(
-                                              children: [
-                                                Text(product.price,
-                                                    style:
-                                                        GoogleFonts.robotoSlab(
-                                                            color:
-                                                                kPrimaryColor,
-                                                            fontSize: 15.sp,
-                                                            fontWeight:
-                                                                FontWeight
-                                                                    .w600)),
-                                                SizedBox(
-                                                  width: 1.w,
+                                        SizedBox(
+                                          width: 3.w,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 4.w),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(
+                                                width: 45.w,
+                                                child: Text(
+                                                  product[index].title,
+                                                  style: GoogleFonts.robotoSlab(
+                                                      color: kTextColor,
+                                                      fontSize: 12.sp,
+                                                      fontWeight:
+                                                          FontWeight.w600),
+                                                  softWrap: false,
+                                                  maxLines: 2,
+                                                  overflow: TextOverflow.ellipsis,
                                                 ),
-                                                Text('\$',
-                                                    style:
-                                                        GoogleFonts.robotoSlab(
-                                                            color:
-                                                                kPrimaryColor))
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  GestureDetector(
-                                    onTap: (() {
-                                      context
-                                          .read<ProductProvider>()
-                                          .removeFromCart(product);
-                                    }),
-                                    child: Icon(
-                                      Icons.delete_forever_outlined,
-                                      size: 7.w,
-                                      color: kPrimaryColor,
+                                              ),
+                                              SizedBox(
+                                                height: 2.w,
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text(product[index].price,
+                                                      style:
+                                                          GoogleFonts.robotoSlab(
+                                                              color:
+                                                                  kPrimaryColor,
+                                                              fontSize: 15.sp,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w600)),
+                                                  SizedBox(
+                                                    width: 1.w,
+                                                  ),
+                                                  Text('\$',
+                                                      style:
+                                                          GoogleFonts.robotoSlab(
+                                                              color:
+                                                                  kPrimaryColor))
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
                                     ),
-                                  )
-                                ],
-                              ),
-                            )),
-                      );
-                    }),
+                                    GestureDetector(
+                                      onTap: (() {
+                                        context
+                                            .read<ProductProvider>()
+                                            .removeFromCart(product[index]);
+                                      }),
+                                      child: Icon(
+                                        Icons.delete_forever_outlined,
+                                        size: 7.w,
+                                        color: kPrimaryColor,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )),
+                        );
+                      }),
+                ),
               ),
-            ),
+        ),
       ),
     ));
   }
